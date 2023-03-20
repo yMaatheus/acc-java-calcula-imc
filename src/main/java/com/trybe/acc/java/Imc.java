2350db033ce0a887930df10e940649b9f31fd2f8
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-03-20 14:10:48 -0300

    Initial commit

---
+++ src/main/java/com/trybe/acc/java/Imc.java
@@ -0,0 +1,26 @@
+package com.trybe.acc.java;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Imc {
+
+	public static void main(String[] args) {
+			// Ler a entrada de um número pelo console	
+			Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+			
+			System.out.println("Digite o peso:");
+			float peso = scan.nextFloat();
+			
+			System.out.println("Digite a aultura:");
+			float altura = scan.nextFloat();
+			
+			// Seu código aqui
+			
+					
+			// Imprime a saída no console. OBS: descomente a próxima linha
+			//System.out.println(imc);
+
+	}
+
+}
